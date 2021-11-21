74711a536989e823df111b14915c08f16cf4511c
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-11-21 18:38:05 +0700

    Kiểm tra user

---
+++ src/Ultils/Auth.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ultils;
+
+import Entity.NhanVien;
+
+/**
+ *
+ * @author pc
+ */
+public class Auth {
+    public static NhanVien user = null;
+     public static boolean isLogin(){
+        
+        return Auth.user != null;
+    }
+    // kiểm tra vai trò
+    public static boolean isManager(){
+        return Auth.isLogin() && user.isVaiTro();
+    }
+}
