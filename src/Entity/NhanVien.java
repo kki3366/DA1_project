58865ad2f81cb7a30313870d63ad9daae7a2b285
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-11-21 18:38:57 +0700

    Thực thể csdl

---
+++ src/Entity/NhanVien.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NhanVien {
+    private String IdNhanVien;
+    private String MatKhau;
+    private String HoTen;
+    private boolean VaiTro;
+    
+}
