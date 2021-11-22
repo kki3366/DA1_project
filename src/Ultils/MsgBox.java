11edcdd32db6e42f19b881aa16aca48c83f32078
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-11-22 17:56:56 +0700

    Root File

---
+++ src/Ultils/MsgBox.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ultils;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author pc
+ */
+public class MsgBox {
+    /* hàm alert
+    @params: Component để gọi đối tượng của frame cha
+    @pargams: message nội dung của Joption
+    dùng để thông báo
+    */
+    public static void alert(Component parent, String message)
+    {
+        JOptionPane.showMessageDialog(parent, message, "Hệ Thống Quản Lý Đào Tạo",JOptionPane.INFORMATION_MESSAGE);
+    }
+     /* hàm confirm
+    @params: Component để gọi đối tượng của frame cha
+    @pargams: message nội dung của Joption
+    dùng để thông báo lựa chọn yes no
+    */
+    public static boolean confirm(Component parent, String message){
+        int result = JOptionPane.showConfirmDialog(parent, message, "Hệ Thống Quản Lý Đào Tạo",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+        return result == JOptionPane.YES_OPTION;
+    }
+      /* hàm prompt
+    @params: Component để gọi đối tượng của frame cha
+    @pargams: message nội dung của Joption
+    dùng để thông báo nhập nội dung vào input
+    */
+    public static String prompt(Component parent, String message){
+        return JOptionPane.showInputDialog(parent, message, "Hệ Thống Quản Lý Đào Tạo", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
