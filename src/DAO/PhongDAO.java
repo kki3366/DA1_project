25cc3fa296f11e0d7aae37c83834738a228d33c9
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-12-07 22:18:09 +0700

    update Phong

--- src/DAO/PhongDAO.java
+++ src/DAO/PhongDAO.java
@@ -5,7 +5,7 @@
  */
 package DAO;
 
-import Entity.KhachHang;
+
 import Entity.Phong;
 import helper.JDBCHelper;
 import java.util.ArrayList;
@@ -23,37 +23,39 @@ import javax.swing.JTextField;
 public class PhongDAO extends DAO<Phong, String> {
 
     private final String INSERT_SQL = "INSERT INTO Phong (IDPhong, TenPhong, TrangThaiPhong, GiaPhong, LoaiPhong) VALUES (?, ?, ?, ?,?)";
-    private final String UPDATE_SQL = "UPDATE Phong SET TenPhong=?, GiaPhong=?, TrangThaiPhong = ?, LoaiPhong=?  WHERE IDPhong=?";
+    private final String UPDATE_SQL = "UPDATE Phong SET TenPhong=?, TrangThaiPhong=?, GiaPhong=?, LoaiPhong=?  WHERE IDPhong=?";
     private final String DELETE_SQL = "DELETE FROM Phong WHERE IDPhong=?";
     private final String SELECT_ALL_SQL = "SELECT * FROM Phong";
     private final String SELECT_BY_ID_SQL = "SELECT * FROM Phong WHERE IDPhong=?";
-    private final String SELECT_ALL_ISACTIVE = "select * from Phong where TrangThaiPhong = 0";
+    private final String UPDATE_ACTIVE ="UPDATE Phong SET TrangThaiPhong = 0 WHERE IDPhong = ?";
+     final String AUTO_ID ="SELECT MAX(IDPhong) as idPhong from Phong";
     private final String SELECT_BY_NAMEROOM = "SELECT * FROM Phong WHERE TenPhong LIKE ?";
-    final String AUTO_ID ="SELECT MAX(IDPhong) as idPhong from Phong";
+    private final String SELECT_ALL_ISACTIVE = "select * from Phong where TrangThaiPhong = 0";
 
     @Override
     public void insert(Phong enity) {
-        JDBCHelper.update(INSERT_SQL, enity.getIDPhong(), enity.getTenPhong(), enity.isTrangThaiPhong(), enity.getGiaPhong(), enity.isLoaiPhong());
+        JDBCHelper.update(INSERT_SQL, enity.getIdPhong(), enity.getTenPhong(), enity.isTrangThaiPhong(), enity.getGiaPhong(),enity.isLoaiPhong());
     }
 
     @Override
     public void update(Phong enity) {
-        JDBCHelper.update(UPDATE_SQL, enity.getTenPhong(),enity.getGiaPhong(),enity.isTrangThaiPhong(),enity.isLoaiPhong(),enity.getIDPhong());
+        JDBCHelper.update(UPDATE_SQL, enity.getTenPhong(),enity.isTrangThaiPhong(),enity.getGiaPhong(),enity.isLoaiPhong(),enity.getIdPhong());
     }
 
     @Override
     public void delete(String id) {
-        JDBCHelper.update(DELETE_SQL, id);
+       JDBCHelper.update(DELETE_SQL, id);
     }
 
     @Override
     public List<Phong> selectAll() {
-        return this.selectBySql(SELECT_ALL_SQL);
+         return this.selectBySql(SELECT_ALL_SQL);
+      
     }
 
     @Override
     public Phong selectById(String id) {
-        List<Phong> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+         List<Phong> list = this.selectBySql(SELECT_BY_ID_SQL, id);
         if (list.isEmpty()) {
             return null;
         }
@@ -66,13 +68,13 @@ public class PhongDAO extends DAO<Phong, String> {
         try {
             ResultSet rs = JDBCHelper.query(sql, args);
             while (rs.next()) {
-                Phong p = Phong.builder()
-                        .IDPhong(rs.getString("IDPhong"))
-                        .TenPhong(rs.getString("TenPhong"))
-                        .TrangThaiPhong(rs.getBoolean("TrangThaiPhong"))
-                        .GiaPhong(rs.getInt("GiaPhong"))
-                        .LoaiPhong(rs.getBoolean("LoaiPhong")).build();
-                list.add(p);
+                Phong enity = new Phong();
+               enity.setIdPhong(rs.getString("IDPhong"));
+               enity.setTenPhong(rs.getString("TenPhong"));
+               enity.setGiaPhong(rs.getInt("GiaPhong"));
+               enity.setTrangThaiPhong(rs.getBoolean("TrangThaiPhong"));
+               enity.setLoaiPhong(rs.getBoolean("LoaiPhong"));
+               list.add(enity);
             }
             rs.getStatement().getConnection().close();
             return list;
@@ -80,22 +82,9 @@ public class PhongDAO extends DAO<Phong, String> {
             throw new RuntimeException(ex);
         }
     }
-
-    public List<Phong> selectAll_isActive() {
-        return this.selectBySql(SELECT_ALL_ISACTIVE);
-    }
-
-    public Phong selectBynameRoom(String nameRoom) {
-        List<Phong> list = this.selectBySql(SELECT_BY_NAMEROOM, nameRoom);
-        if (list.isEmpty()) {
-            return null;
-        }
-        return list.get(0);
-    }
     
-     
     public void AutoIDPhong(JTextField jtxf){
-        try {
+         try {
             ResultSet rs;
             rs = JDBCHelper.query(AUTO_ID);
             rs.next();
@@ -115,6 +104,23 @@ public class PhongDAO extends DAO<Phong, String> {
             Logger.getLogger(KhachHangDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
+    
+    
+    public void PhongStatus(String idRoom){
+        JDBCHelper.update(UPDATE_ACTIVE, idRoom);
+    }
+    
+    public Phong selectBynameRoom(String nameRoom) {
+        List<Phong> list = this.selectBySql(SELECT_BY_NAMEROOM, nameRoom);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+    
+     public List<Phong> selectAll_isActive() {
+        return this.selectBySql(SELECT_ALL_ISACTIVE);
+    }
 
 }
