cb5e4a8199dcae0c1cc2a3e9093bfdc945999f8c
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-12-07 11:33:56 +0700

    update Sanpham enity

--- src/Entity/SanPham.java
+++ src/Entity/SanPham.java
@@ -10,71 +10,133 @@ package Entity;
  * @author WINDOW
  */
 public class SanPham {
-    int maSP;
-    String tenSP;
-    String NgTao;
-    double soLuong;
-    double giaSP;
-    String moTa;  
-
-    public SanPham(int maSP, String tenSP, String NgTao, double soLuong, double giaSP, String moTa) {
-        this.maSP = maSP;
-        this.tenSP = tenSP;
-        this.NgTao = NgTao;
-        this.soLuong = soLuong;
-        this.giaSP = giaSP;
-        this.moTa = moTa;
-    }
-
-    public String getNgTao() {
-        return NgTao;
-    }
-
-    public void setNgTao(String NgTao) {
-        this.NgTao = NgTao;
-    }
-
-
-    public double getSoLuong() {
-        return soLuong;
-    }
-
-    public void setSoLuong(double soLuong) {
-        this.soLuong = soLuong;
-    }
+//    int maSP;
+//    String tenSP;
+//    String NgTao;
+//    double soLuong;
+//    double giaSP;
+//    String moTa;  
+//
+//    public SanPham(int maSP, String tenSP, String NgTao, double soLuong, double giaSP, String moTa) {
+//        this.maSP = maSP;
+//        this.tenSP = tenSP;
+//        this.NgTao = NgTao;
+//        this.soLuong = soLuong;
+//        this.giaSP = giaSP;
+//        this.moTa = moTa;
+//    }
+//
+//    public String getNgTao() {
+//        return NgTao;
+//    }
+//
+//    public void setNgTao(String NgTao) {
+//        this.NgTao = NgTao;
+//    }
+//
+//
+//    public double getSoLuong() {
+//        return soLuong;
+//    }
+//
+//    public void setSoLuong(double soLuong) {
+//        this.soLuong = soLuong;
+//    }
+//
+//    public SanPham() {
+//    }
+//
+//    public int getMaSP() {
+//        return maSP;
+//    }
+//
+//    public void setMaSP(int maSP) {
+//        this.maSP = maSP;
+//    }
+//
+//    public String getTenSP() {
+//        return tenSP;
+//    }
+//
+//    public void setTenSP(String tenSP) {
+//        this.tenSP = tenSP;
+//    }
+//
+//    public double getGiaSP() {
+//        return giaSP;
+//    }
+//
+//    public void setGiaSP(double giaSP) {
+//        this.giaSP = giaSP;
+//    }
+//
+//    public String getMoTa() {
+//        return moTa;
+//    }
+//
+//    public void setMoTa(String moTa) {
+//        this.moTa = moTa;
+//    }   
+     private int IdSanPham;
+    private String TenSanPham;
+    private int GiaSanPham;
+    private String MoTaSanPham;
+    private String IdNhanVien;
 
     public SanPham() {
     }
 
-    public int getMaSP() {
-        return maSP;
+    public SanPham(int IdSanPham, String TenSanPham, int GiaSanPham, String MoTaSanPham, String IdNhanVien) {
+        this.IdSanPham = IdSanPham;
+        this.TenSanPham = TenSanPham;
+        this.GiaSanPham = GiaSanPham;
+        this.MoTaSanPham = MoTaSanPham;
+        this.IdNhanVien = IdNhanVien;
     }
 
-    public void setMaSP(int maSP) {
-        this.maSP = maSP;
+    
+    
+    
+    
+    
+    public int getIdSanPham() {
+        return IdSanPham;
     }
 
-    public String getTenSP() {
-        return tenSP;
+    public void setIdSanPham(int IdSanPham) {
+        this.IdSanPham = IdSanPham;
     }
 
-    public void setTenSP(String tenSP) {
-        this.tenSP = tenSP;
+    public String getTenSanPham() {
+        return TenSanPham;
     }
 
-    public double getGiaSP() {
-        return giaSP;
+    public void setTenSanPham(String TenSanPham) {
+        this.TenSanPham = TenSanPham;
     }
 
-    public void setGiaSP(double giaSP) {
-        this.giaSP = giaSP;
+    public int getGiaSanPham() {
+        return GiaSanPham;
     }
 
-    public String getMoTa() {
-        return moTa;
+    public void setGiaSanPham(int GiaSanPham) {
+        this.GiaSanPham = GiaSanPham;
     }
 
-    public void setMoTa(String moTa) {
-        this.moTa = moTa;
-    }   
+    public String getMoTaSanPham() {
+        return MoTaSanPham;
+    }
+
+    public void setMoTaSanPham(String MoTaSanPham) {
+        this.MoTaSanPham = MoTaSanPham;
+    }
+
+    public String getIdNhanVien() {
+        return IdNhanVien;
+    }
+
+    public void setIdNhanVien(String IdNhanVien) {
+        this.IdNhanVien = IdNhanVien;
+    }
+    
 }
