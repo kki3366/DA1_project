840a99e788bbb0b331350f5f8cbf2c98a66859d5
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-12-08 16:29:30 +0700

    ddd

---
+++ src/Entity/ChiTietHoaDon.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author pc
+ */
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChiTietHoaDon {
+    private int IDChiTietHoaDon;
+    private int SoLuong;
+    private int IDHoaDon;
+    private int IDSanPham;
+    private int DonGiaChiTietHoaDon;
+   
+}
