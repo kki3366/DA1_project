724af2f8ee77368d716b41f8652d4838b51c0704
khoavdpc00466 <pc@LAPTOP-NFE30ABT>
2021-12-08 20:17:45 +0700

    update SanPhamDao

--- src/DAO/SanPhamDAO.java
+++ src/DAO/SanPhamDAO.java
@@ -17,85 +17,28 @@ import java.util.List;
  * @author WINDOW
  */
 public class SanPhamDAO extends DAO<SanPham, Integer>{
-//    String INSERT_SQL = "INSERT INTO SANPHAM(IDSanPham,TenSanPham, GiaSanPham, MoTaSanPham, IDDVT, IDNhanVien)"+"VALUES (?,?,?,?,?,?,?)";
-//    String UPDATE_SQL = "UPDATE SANPHAM SET TenSanPham = ?,GiaSanPham= ?,MoTaSanPham=? where IDSanPham = ?";
-//    String DELETE_SQL = "DELETE FROM SANPHAM WHERE IDSanPham=?";
-//    String SELECT_ALL_SQL = "SELECT * FROM SanPham";
-//    String SELECT_BY_ID_SQL = "SELECT * FROM SANPHAM WHERE IDSanPham=?";
-//    public void insert(SanPham entity){
-//        JDBCHelper.update(INSERT_SQL, entity.getMaSP(),entity.getTenSP(),entity.getGiaSP(),entity.getMoTa());
-//    }
-//    public void update(SanPham entity){
-//    JDBCHelper.update(UPDATE_SQL, entity.getTenSP(),entity.getGiaSP(),entity.getMoTa(),entity.getMaSP());
-//    }
-//    public void delete(String id){
-//    JDBCHelper.update(DELETE_SQL, id);
-//    }
-// 
-//
-//    public List<SanPham> selectAll() {
-//        return this.selectBySql(SELECT_ALL_SQL);
-//    }
-//
-//   
-//    public SanPham selectById(Integer id) {
-//        List<SanPham> list = this.selectBySql(SELECT_BY_ID_SQL, id); // đang lỗi 
-//        if (list.isEmpty()) {
-//            return null;
-//        }
-//        return list.get(0);
-//    }
-//    
-//    // kiểm tra lại đoạn này
-//    public List<SanPham> selectBySql(String sql, Object... args) {
-//        List<SanPham> list = new ArrayList<>();
-//        try {
-//            ResultSet result = JDBCHelper.query(sql, args);
-//            while (result.next()) {
-//                SanPham entity = new SanPham();
-//                entity.setMaSP(result.getInt("MAKH"));                        
-//                entity.setTenSP(result.getString("GHICHU"));
-//                entity.setNgTao(result.getString("NguoiTao"));
-//                entity.setGiaSP(result.getDouble("GiaSanPham"));
-//                entity.setSoLuong(result.getDouble("SoLuong"));
-//                entity.setMoTa(result.getString("MaNV"));                       
-//                list.add(entity);
-//            }
-//            result.getStatement().getConnection().close();
-//            return list;
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    public List<SanPham> selectByDonViTinh(String maCD) {
-//        String sql = "SELECT * FROM DONVITINH WHERE IDDVT = ?";
-//        return this.selectBySql(sql, maCD);
-//    }
-//
-//    public SanPham selectById(String SP) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//    
-//   
-//    
-     final String SELECT_ALL = "select * from SanPham";
     
+    String INSERT_SQL = "INSERT INTO SANPHAM(IDSanPham,TenSanPham, GiaSanPham, MoTaSanPham, IDDVT, IDNhanVien)"+"VALUES (?,?,?,?,?,?,?)";
+    String UPDATE_SQL = "UPDATE SANPHAM SET TenSanPham = ?,GiaSanPham= ?,MoTaSanPham=? where IDSanPham = ?";
+    String DELETE_SQL = "DELETE FROM SANPHAM WHERE IDSanPham=?";
+    String SELECT_ALL_SQL = "SELECT * FROM SanPham";
+    String SELECT_BY_ID_SQL = "SELECT * FROM SANPHAM WHERE IDSanPham=?";
+     final String SELECT_ALL = "select * from SanPham";
     final String Select_All_By_Id = "select * from SanPham where TenSanPham LIKE ?";
     
     @Override
     public void insert(SanPham enity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JDBCHelper.update(INSERT_SQL, enity.getIdSanPham(),enity.getTenSanPham(),enity.getGiaSanPham(),enity.getMoTaSanPham(),enity.getIdNhanVien());
     }
     
     @Override
     public void update(SanPham enity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JDBCHelper.update(UPDATE_SQL,enity.getTenSanPham(),enity.getGiaSanPham(),enity.getMoTaSanPham(),enity.getIdNhanVien(),enity.getIdSanPham());
     }
     
     @Override
     public void delete(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         JDBCHelper.update(DELETE_SQL, id);
     }
     
     @Override
@@ -105,8 +48,13 @@ public class SanPhamDAO extends DAO<SanPham, Integer>{
     
     @Override
     public SanPham selectById(Integer id) {
-         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<SanPham> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);    
     }
+
     
     @Override
     public List<SanPham> selectBySql(String sql, Object... args) {
